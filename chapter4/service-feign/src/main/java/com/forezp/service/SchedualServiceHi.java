bb98a4e78a594aa72a10aad820a0b55aa60c3ce4
029ye <370505634@qq.com>
2017-10-12 10:24:28 +0800

    open feign hystrix

--- chapter4/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
+++ chapter4/service-feign/src/main/java/com/forezp/service/SchedualServiceHi.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by fangzhipeng on 2017/4/6.
  */
-@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)/*使用hystrix断路器，需设置fallback属性，且值类要继承自己*/
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
